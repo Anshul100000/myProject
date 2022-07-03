a06b61295323d5baa2c3b22cb1eb42860a81169e
Anshul100000 <anshulrathore01234>
2022-07-03 11:15:35 +0530

    emp use case 2

---
+++ empwage/src/uc2.java
@@ -0,0 +1,17 @@
+
+public class uc2 {
+	public static void main(String[]args) {
+		int isfulltime = 1;
+		int wageperhour = 20;
+		int emphrs = 0 , empdailywage = 0
+				double empcheck = Math.floor(Math.random()*10 % 2);
+		if (empcheck == isfulltime) {
+			emphrs = 8
+			empdailywage = (emphrs*wageperhour);
+			System.out.println("employe is present :- "+ empdailywage);
+		}
+		else {
+			System.out.println("employe is absent:-"+ empdailywage );
+		}
+	}
+}
